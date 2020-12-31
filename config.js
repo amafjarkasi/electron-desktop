092d12f39251de405d9b4676ed059ec6e8606bdc
amafjarkasi <amafjarkasi@gmail.com>
2020-12-30 23:10:48 -0500

    latest updates

---
+++ config.js
@@ -0,0 +1,26 @@
+//WebToDesk v1.0.0
+//Sameera Damith (CodeStack)
+//damith.sameera1@gmail.com
+
+const config = {
+    //Main Application URL
+    'websiteUrl' : 'https://example.com',
+
+    //Application Name
+    'appName' : 'JBD Team Panel',
+
+    //Application window width and height
+    'width' : 1080,
+    'height' : 600,
+    'minWidth' : 1080,
+    'minHeight' : 600,
+
+    //Hide elements by ID
+    'hideElementsId' : [],
+
+    //Hide elements by Class
+    'hideElementsClass' : [],
+
+}
+
+module.exports = config
